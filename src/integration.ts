0769cf139e438a08fc5dabd597d4f7d91f83a09f
Matthias Volk <mv@infrablue.org>
2025-05-03 17:42:19 +0200

    Runge-Kutta integrator and Lorenz system

---
+++ src/integration.ts
@@ -0,0 +1,112 @@
+type OdeFunction = (_t: number, x: Float32Array, xdot: Float32Array) => void;
+
+const rho = 28;
+const sigma = 10;
+const beta = 8 / 3;
+
+export function lorenz(_t: number, x: Float32Array, xdot: Float32Array) {
+  xdot[0] = sigma * (x[1] - x[0]);
+  xdot[1] = x[0] * (rho - x[2]) - x[1];
+  xdot[2] = x[0] * x[1] - beta * x[2];
+}
+
+export interface RungeKuttaParams {
+  f: OdeFunction;
+  x0: Float32Array;
+  t0: number;
+  dt: number;
+  eps: number;
+}
+
+export class RungeKuttaIntegrator {
+  private t: number;
+  private h: number;
+
+  private readonly dims: number;
+  private readonly eps: number;
+  private readonly f: OdeFunction;
+  private readonly x: Float32Array;
+  private readonly xdot: Float32Array;
+  private readonly tmp: Float32Array;
+  private readonly err: Float32Array;
+  private readonly ks: Float32Array[];
+  private readonly nstages: number;
+
+  static tab = {
+    A: new Float32Array([0, 1 / 4, 3 / 8, 12 / 13, 1, 1 / 2]),
+    B: new Float32Array([
+      0, 0, 0, 0, 0,
+      1 / 4, 0, 0, 0, 0,
+      3 / 32, 9 / 32, 0, 0, 0,
+      1932 / 2197, -7200 / 2197, 7296 / 2197, 0, 0,
+      439 / 216, -8, 3680 / 513, -845 / 4104, 0,
+      -8 / 27, 2, -3544 / 2565, 1859 / 4104, -11 / 40
+    ]),
+    CH: new Float32Array([16 / 135, 0, 6656 / 12825, 28561 / 56430, -9 / 50, 2 / 55]),
+    CT: new Float32Array([-1 / 360, 0, 128 / 4275, 2197 / 75240, -1 / 50, -2 / 55]),
+  };
+
+  constructor(params: RungeKuttaParams) {
+    this.t = params.t0;
+    this.f = params.f;
+    this.h = params.dt;
+    this.x = params.x0.slice()
+    this.eps = params.eps;
+    this.nstages = 6;
+
+    this.dims = this.x.length;
+    this.xdot = new Float32Array(this.dims);
+    this.f(this.t, this.x, this.xdot);
+
+    this.ks = [];
+    for (let i = 0; i < this.nstages; i++) {
+      this.ks.push(new Float32Array(this.dims));
+    }
+
+    this.tmp = new Float32Array(this.dims);
+    this.err = new Float32Array(this.dims);
+  }
+
+  next(): { t: number; x: Float32Array } {
+    for (let k = 0; k < this.nstages; k++) {
+      this.tmp.set(this.x);
+      for (let l = 0; l < k; l++) {
+        for (let i = 0; i < this.dims; i++) {
+          this.tmp[i] += this.h * RungeKuttaIntegrator.tab.B[k * 5 + l] * this.ks[l][i];
+        }
+      }
+      this.f(this.t + this.h * RungeKuttaIntegrator.tab.A[k], this.tmp, this.ks[k]);
+    }
+
+    this.err.fill(0);
+    this.tmp.set(this.x);
+    for (let k = 0; k < this.nstages; k++) {
+      for (let i = 0; i < this.dims; i++) {
+        this.tmp[i] += this.h * RungeKuttaIntegrator.tab.CH[k] * this.ks[k][i];
+        this.err[i] += this.h * RungeKuttaIntegrator.tab.CT[k] * this.ks[k][i];
+      }
+    }
+
+    const te = Math.sqrt(this.err.reduce((mag, v) => mag + v * v, 0))
+    const hn = Math.pow(this.eps / te, 0.2) * this.h * 0.9;
+
+    if (te > this.eps) {
+      this.h = hn;
+      return this.next();
+    }
+
+    this.t += this.h;
+    this.h = hn;
+    this.x.set(this.tmp);
+
+    return this.state();
+  }
+
+  state() {
+    return {
+      t: this.t,
+      x: this.x,
+    };
+  }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import './style.css'
 
 import * as THREE from 'three';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { lorenz, RungeKuttaIntegrator, RungeKuttaParams } from './integration';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id="scene">
   </div>
 `
-
 const width = 800;
 const height = 600;
 
@@ -24,28 +24,106 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0, 0);
 controls.update();
 
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({
-  color: 0x00ff00,
-});
-
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-camera.position.z = 5;
-
 const pointLight = new THREE.PointLight(0xffffff, 1.0);
-pointLight.position.set(1, 1, 0);
+pointLight.position.set(0, 0, 0);
 scene.add(pointLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+scene.add(directionalLight);
+
+interface LorenzVisualizerParams {
+  rungeKuttaParams: RungeKuttaParams,
+  radius: number;
+  color: THREE.ColorRepresentation;
+  count: number;
+}
+
+class LorenzVisualizer {
+  private n = 0;
+
+  private readonly integrator;
+  private readonly geometry;
+  private readonly material;
+  private readonly mesh;
+  private readonly dummy;
+  
+  constructor(params: LorenzVisualizerParams) {
+    this.geometry = new THREE.SphereGeometry(params.radius);
+    this.material = new THREE.MeshPhongMaterial({
+      color: params.color,
+    });
+
+    this.mesh = new THREE.InstancedMesh(this.geometry, this.material, params.count);
+    this.mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+
+    this.dummy = new THREE.Object3D();
+
+    this.integrator = new RungeKuttaIntegrator(params.rungeKuttaParams);
+  }
+
+  addTo(scene: THREE.Scene) {
+    scene.add(this.mesh);
+  }
+
+  update() {
+    const { x } = this.integrator.next();
+
+    this.dummy.position.set(x[0], x[1], x[2]);
+    this.dummy.updateMatrix();
+    this.mesh.setMatrixAt(this.n % this.mesh.count, this.dummy.matrix);
+    this.mesh.instanceMatrix.needsUpdate = true;
+    this.mesh.computeBoundingSphere();
+    this.n += 1;
+  }
+}
+
+camera.position.z = 55;
 
 function render() {
   renderer.render(scene, camera);
 }
 
-render();
-
 controls.addEventListener("change", render);
+
+const lorenz1 = new LorenzVisualizer({
+  rungeKuttaParams: {
+    f: lorenz,
+    x0: new Float32Array([1, 2, 1]),
+    t0: 0,
+    dt: 0.01,
+    eps: 1e-6,
+  },
+  color: 0xff0000,
+  count: 1024,
+  radius: 0.3,
+});
+
+const lorenz2 = new LorenzVisualizer({
+  rungeKuttaParams: {
+    f: lorenz,
+    x0: new Float32Array([1.1, 2, 1]),
+    t0: 0,
+    dt: 0.01,
+    eps: 1e-6,
+  },
+  color: 0x00ff00,
+  count: 1024,
+  radius: 0.3,
+});
+
+lorenz1.addTo(scene);
+lorenz2.addTo(scene);
+
+function animate() {
+  setTimeout(() => {
+    requestAnimationFrame(animate);
+  }, 50);
+  lorenz1.update();
+  lorenz2.update();
+  render();
+}
+
+animate();
