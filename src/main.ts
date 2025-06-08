6e98844d0adfa691cd4bc596c737f446af45aa74
Matthias Volk <mv@infrablue.org>
2025-06-08 10:51:02 +0200

    Remove sphere and traces visualizers

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { LorenzSystem, RoesslerSystem, ThomasSystem } from './systems';
-import { PointCloud, Spheres, Traces } from './visualizers';
-import { RungeKuttaIntegrator } from './integration';
+import { PointCloud } from './visualizers';
 import colormaps from './colormaps';
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import Stats from 'stats-gl';
@@ -62,11 +61,9 @@ function render() {
 controls.addEventListener("change", render);
 
 const controllers: any[] = [];
-const instances: Spheres[] | Traces[] = []
 const parameters = {
   system: "lorenz",
   colormap: "red",
-  tail: 1,
   interval: 50,
   iterations: 1,
   reset: () => computeShader.reset(),
@@ -81,9 +78,8 @@ const computeShader = new ComputeShader({
 })
 
 let colormap = colormaps.get(parameters.colormap)!;
-const count = 0; // 2000;
 
-function init(count: number, system: string) {
+function init(system: string) {
   let odeSystem;
 
   if (system === "lorenz") {
@@ -111,55 +107,15 @@ function init(count: number, system: string) {
     });
     controllers.push(controller);
   }
-
-  for (let i = 0; i < count; i++) {
-    const integrator = new RungeKuttaIntegrator({
-      f: odeSystem.func.bind(odeSystem),
-      // x0: new Float32Array([1 + i, 2 - i, i * 10]),
-      x0: new Float32Array([25 - Math.random() * 50, 25 - Math.random() * 50, 25 - Math.random() * 50]),
-      t0: 0,
-      dt: 0.0001,
-      eps: 1e-6,
-    });
-
-    colormap.mirror = true;
-    const spheres = new Spheres({
-      integrator: integrator,
-      colorOptions: {
-        type: "constant",
-        color: colormap.sample((i + 0.5) / count),
-        // type: "colormap",
-        // colormap: colormap,
-      },
-      count: parameters.tail,
-      radius: 0.3,
-    });
-    scene.add(spheres);
-    instances.push(spheres);
-  }
 }
 
 gui.add(parameters, "system", ["lorenz", "roessler", "thomas"]).onChange((system) => {
-  for (let i = 0; i < count; i++) {
-    const sphere = instances.pop();
-    if (!sphere) {
-      break;
-    }
-
-    while (controllers.length > 0) {
-      const c = controllers.pop();
-      c.destroy();
-    }
-
-    scene.remove(sphere)
-  }
-
   while (controllers.length > 0) {
     const c = controllers.pop();
     c.destroy();
   }
 
-  init(count, system);
+  init(system);
 });
 
 gui.add(parameters, "colormap", [...colormaps.keys()]).onChange((key) => {
@@ -169,37 +125,6 @@ gui.add(parameters, "colormap", [...colormaps.keys()]).onChange((key) => {
 
   colormap = colormaps.get(key)!;
   colormap.mirror = true;
-
-  for (let i = 0; i < count; i++) {
-    switch (instances[i].colorOptions.type) {
-      case "constant":
-        instances[i].colorOptions = {
-          type: "constant",
-          color: colormap.sample((i + 0.5) / count)
-        };
-        break;
-      case "colormap":
-        instances[i].colorOptions = {
-          type: "colormap",
-          colormap: colormap
-        };
-        break;
-    }
-
-    if (paused) {
-      render();
-    }
-  }
-});
-
-gui.add(parameters, "tail", 1, 1000, 1).onChange((tail) => {
-  for (let i = 0; i < count; i++) {
-    instances[i].setCount(tail);
-
-    if (paused) {
-      render();
-    }
-  }
 });
 
 gui.add(parameters, "interval", 0, 500, 10);
@@ -217,7 +142,7 @@ gui.add(parameters, "showArrows").onChange((showArrows) => {
   }
 })
 
-init(count, parameters.system);
+init(parameters.system);
 
 const pointCloud = new PointCloud({
   height: 256,
@@ -232,7 +157,6 @@ function animate() {
       requestAnimationFrame(animate);
     }, parameters.interval);
   }
-  instances.forEach((instance) => instance.update());
 
   const activeTexture = computeShader.update()
   pointCloud.setUniform('positions', activeTexture)
